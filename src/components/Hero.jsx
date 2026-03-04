3acda0053a0adc7efbb4b1a54800251d09d8c124
ErnestMakarov <ernest.makarov@tptlive.ee>
2026-03-04 22:41:41 +0200

    SEO BOOST, meta and h1 fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,8 @@ const Hero = () => {
     <main className="hero" id='about'>
         <div className="container">
             <div className="hero-container">
-                <p className='hero-text' data-aos="fade-down">MODERN WEB DEVELOPMENT WITH REACT</p>
+                <h1 className='hero-text' data-aos="fade-down">Web Developer in Estonia<br/>
+                  Building Modern React Websites</h1>
                 <h2 data-aos="fade-down" >Transforming <span>Concepts</span> into Seamless User Experiences</h2>
                 <p data-aos="fade-up" className='hero-second-text'>I design and develop fast, accessible websites that help businesses grow.</p>
                 <a data-aos="fade-up" className='hero-btn' href="#contact">Get a website <GoArrowUpRight /></a>
